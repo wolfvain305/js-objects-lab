c49fc32685dab39906e76712cabb37c1de79676f
wolfvain305 <dwolf0305@gmail.com>
2024-05-19 22:21:29 -0400

    exercise 8

--- app.js
+++ app.js
@@ -17,14 +17,13 @@ const game = {
     ],
   }
 
-  /*Exercise 1
-    console.dir(pokemon, {maxArrayLength: null})
-  */
+  console.log('Exercise 1 Results:');
+     console.dir(pokemon, {maxArrayLength: null})
+  
 
-  /*Exercise 2
-
+    console.log('Exercise 2 Results:');
     console.log(game)
-  */
+  
 /*
 Exercise 3
 1. Add a new property to the `game` object. Let's call it "difficulty".
@@ -33,8 +32,9 @@ Exercise 3
 
 Solve Exercise 3 here:
 */
+console.log('Exercise 3 Results:');
 game.difficulty = "Medium"
-
+console.log(game)
 
 
 /*
@@ -45,9 +45,9 @@ Exercise 4
 
 Solve Exercise 4 here:
 */
-
+console.log('Exercise 4 Results:');
 game.party.push (pokemon[24])
-
+console.log(game)
 /*
 Exercise 5
 1. Choose three more Pokémon from the `pokemon` array and add them to your party.
@@ -56,8 +56,65 @@ Exercise 5
 
 Solve Exercise 5 here:
 */
-game.party.push (pokemon[150], pokemon[149], pokemon[3])
+console.log('Exercise 5 Results:');
+game.party.push ( pokemon[149],pokemon[150], pokemon[3])
+console.log(game)
+
+/*
+Exercise 6
+1. Arrange the Pokémon in `game.party` by their HP. The one with the highest HP should come first.
+2. You'll need to use the `.sort()` method. How does the compare function work in sorting numbers?
 
 
+Solve Exercise 6 here:
+*/
+const sortByHP = (a, b) => {
+  return b.hp - a.hp
+}
+game.party.sort(sortByHP)
 
+console.log('Exercise 6 Results:');
+console.log(game)
+
+/*
+Exercise 7
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 7 here:
+*/
+
+game.gyms.forEach(gym =>{
+  if (gym.difficulty <= 3){
+    gym.completed= true;
+  }
+})
+
+console.log('Exercise 7 Results:');
+console.log(game)
+
+/*
+Exercise 8
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. Remember that you're working with an array of objects - what array method is ideal for replacing one element with another? 
+
+Solve Exercise 8 here:
+*/
+
+const Evolve = game.party.findIndex(pokemon => pokemon.name === "Pikachu")
+if (Evolve !== -1) {
+  game.party.splice(Evolve, 1, pokemon[25])
+}
+
+
+console.log('Exercise 8 Results:');
 console.log(game)
